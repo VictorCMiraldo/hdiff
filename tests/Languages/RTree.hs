5cad0899cb67820976cab4e8b0c27ae9716511c7
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-10-15 13:35:52 +0200

    Brought in hspec to our test-suite. Diff algo is apparently wrong.

--- tests/Languages/RTree.hs
+++ tests/Languages/RTree.hs
@@ -13,20 +13,45 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 module Languages.RTree where
 
+import Data.Type.Equality
+
 import Generics.MRSOP.Base
 import Generics.MRSOP.Util
 import Generics.MRSOP.TH
 import Generics.MRSOP.Digems.Digest
+import Generics.MRSOP.Digems.Renderer
 
 import Control.Monad
-import Control.Monad.Identity
 import Test.QuickCheck
 
 data RTree = String :>: [RTree]
   deriving (Eq , Show)
 
+height :: RTree -> Int
+height (n :>: []) = 0
+height (n :>: ns) = 1 + maximum (map height ns)
+
+data WKon = WString
+
+data W :: WKon -> * where
+  W_String  :: String  -> W WString
+
+instance Eq1 W where
+  eq1 (W_String s)  (W_String ss) = s == ss
+
+instance Digestible1 W where
+  digest1 (W_String s)  = hashStr s
+
+instance Show1 W where
+  show1 (W_String s)  = s
+
+instance TestEquality W where
+  testEquality (W_String _)  (W_String _)  = Just Refl
+
+deriveFamilyWith ''W [t| RTree |]
+
 genConName :: Gen String
-genConName = resize 3 (listOf (choose ('a' , 'z')))
+genConName = (:[]) <$> choose ('a' , 'm')
 
 genTree :: Int -> Gen RTree
 genTree h
@@ -37,6 +62,41 @@ genTree h
       x <- choose (0, 4)
       vectorOf x $ genTree (h-1)
 
+insertAt :: Int -> a -> [a] -> [a]
+insertAt 0 x xs       = x : xs
+insertAt n x (y : ys) = x : insertAt (n-1) x ys
+
+genInsHere :: RTree -> Gen RTree
+genInsHere t = do
+  n  <- genConName
+  k  <- choose (0 , 3)
+  ns <- vectorOf k (genTree (height t))
+  k' <- if length ns == 0
+        then return 0
+        else choose (0 , length ns - 1)
+  return (n :>: insertAt k' t ns)
+
+genSimilarTrees :: Int -> Gen (RTree , RTree)
+genSimilarTrees h = do
+  t  <- genTree h
+  t' <- go (height t) 1 t 
+  return (t , t')
+  where
+    go :: Int -> Int -> RTree -> Gen RTree
+    go ht ch (n :>: ns) = do
+      ns' <- mapM (go ht (ch + 1)) ns
+      n'  <- frequency [ (ht , return n)
+                       , (ch  , genConName) ]
+      frequency $ [ (ch , genInsHere (n' :>: ns'))
+                  , (ht , return (n' :>: ns'))
+                  ] ++ (if length ns > 0
+                      then [ (ch , elements ns') ] -- genDelHere 
+                      else [] )
+
+instance Arbitrary RTree where
+  arbitrary = sized $ \n -> choose (1 , n `div` 2) >>= genTree
+
+{-
 genTree' :: Int -> Gen RTree
 genTree' h
   | h >= 3    = choose (h - 3 , h + 3) >>= genTree
@@ -57,100 +117,7 @@ genDisjTrees rt = oneof [ ins rt , del rt , mod rt ]
       n  <- genConName
       k  <- choose (1 , 3)
       ts <- vectorOf k (genTree' (height t))
+-}
        
-
-
-genMutatedTree :: RTree -> Gen RTree
-genMutatedTree = undefined
-
-codeltaMapM :: (Monad m) => (a -> m b) -> Either a a -> m (Either b b)
-codeltaMapM f (Left x)  = Left <$> f x
-codeltaMapM f (Right x) = Right <$> f x
-
-onlyOne :: [a] -> Gen [Either a a]
-onlyOne xs = go xs >>= oneof . map return
-  where
-    go []     = return []
-    go (x:xs) = do
-      xs' <- go xs
-      return ((Right x : map Left xs) : map ((Left x) :) xs')
-
-{-
-
-option :: Float -> (a -> Gen a) -> a -> Gen a
-option thr f x = do
-  let thrI :: Int = round (thr * 100)
-  t <- choose (0 , 100)
-  if t <= thrI
-  then f x
-  else return x
-
--- |Given a tree and a final result, give two trees
---  that could be merged into it.
-genMerge :: RTree -> RTree -> Gen (RTree , RTree)
-genMerge o ab
-  = oneof [ ins o ab
-          , del o ab
-          , mod o ab
-          ]
-  where
-    getRight (Right x : _)  = x
-    getRight (_       : xs) = getRight xs
-    
-    ins o (ab :>: [])  = return (o , ab :>: [])
-    ins o (ab :>: abs) = do
-      abs' <- onlyOne abs
-      let sel = getRight abs'
-      (selA , selB) <- genMerge o sel
-      let bs = map (either id (const selB)) abs'
-      return (selA , ab :>: bs)
-
-    del (o :>: []) ab = return (o :>: [] , ab)
-    del (o :>: os) ab = do
-      os' <- onlyOne os
-      let sel = getRight os'
-      (selA , selB) <- genMerge sel ab
-      let as = map (either id (const selA)) os'
-      return (o :>: as , selB)
-
-    mod (o :>: os) (ab :>: abs)
-      | length os /= length abs = oneof [ del (o :>: os)  (ab :>: abs)
-                                        , ins (o :>: os)  (ab :>: abs)
-                                        , ins (ab :>: os) (o  :>: abs)
-                                        , del (ab :>: os) (o  :>: abs) ]
-      | otherwise = do
-          (as , bs) <- unzip <$> mapM (uncurry genMerge) (zip os abs)
-          elements [ (o :>: as , ab :>: bs) , (ab :>: as , o :>: bs) ]
-      
--}
-
-t1 = "Add" :>: [ "Var" :>: [ "x" :>: [] ] , "Var" :>: [ "y" :>: [] ] ]
-t2 = "Sub" :>: [ "LOL" :>: [ "x" :>: [] ] ]
-                   
-
-{-
-
-  do
-  (a  , b)  <- elements [(o , ab) , (ab , o)]
-  (as , bs) <- unzip <$> mapM (uncurry genMerge) (zip os abs)
--}
-
-{-
-genTreeMerge :: RTree -> Gen (RTree , RTree)
-genTreeMerge (c :>: cs) = do
-  cs' <- genPartition cs >>= mapM (codeltaMapM (option 0.5 genMutatedTree))
-  let csA = map keepLefts  (zip cs cs')
-  let csB = map keepRights (zip cs cs')
-  _
-  where
-    keepLefts :: (a , Either a x) -> a
-    keepLefts  (x , yx) = either id (const x) yx
-
-    keepRights :: (a , Either x a) -> a
-    keepRights (x , xy) = either (const x) id xy
--}
-
-
-
 
 
