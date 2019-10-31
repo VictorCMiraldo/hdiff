6082305633c98592d1706744d73422edc6f0d003
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2019-10-31 09:14:47 +0100

    Comments with many situations side by side

--- src/Data/HDiff/Change/Merge.hs
+++ src/Data/HDiff/Change/Merge.hs
@@ -59,6 +59,124 @@ decide (Hole' p)  cq       = registerL p cq
 decide cp        (Hole' q) = registerR cp q
 decide cp         cq       = lift Nothing
 
+
+{- Examples from MergeSpec.hs
+
+-- Apparently; registerLR is only ok
+-- if either p or q is a copy.
+-- 
+registerLR
+  p = `NA_K a[3]
+    ; `NA_K a[3]
+  q = `NA_K a[2]
+    ; `NA_K a[2]
+
+-- This really says: p old [1] is ((:>:) b [])
+-- and p new [1] is ((:>:) b' []). This enables us to
+-- store this information to maintain coherence.
+registerL
+  p = `NA_I (Const 1)
+    ; `NA_I (Const 1)
+  q = ((:>:)  b [] )
+    ; ((:>:)  b' [] )
+
+-- Again; a simple copy over some change.
+registerLR
+  p = `NA_I (Const 0)
+    ; ((:)  ((:>:)  c [] ) `NA_I (Const 0))
+  q = `NA_I (Const 0)
+    ; `NA_I (Const 0)
+
+-------------------
+-------------------
+-------------------
+
+-- This is nastier; the left reached a change and the right
+-- has changes inside of it. We want to apply p to q; why? how?
+registerL
+  p = ((:>:)  b ((:)  `NA_I (Const 3) ((:)  ((:>:)  . [] ) [] )))
+    ; `NA_I (Const 3)
+  q = ((:>:)  `NA_K b[4] ((:)  ((:>:)  `NA_K b[5] ((:)  ((:>:)  `NA_K u[6] ((:)  ((:>:)  3 [] ) [] )) `NA_I (Const 0))) `NA_I (Const 2)))
+    ; ((:>:)  `NA_K b[4] ((:)  ((:>:)  `NA_K b[5] ((:)  ((:>:)  `NA_K u[6] ((:)  ((:>:)  4 [] ) [] )) ((:)  ((:>:)  u `NA_I (Const 0)) [] ))) `NA_I (Const 2)))
+
+-----------------
+-----------------
+-----------------
+
+-- This one is very simple; one LR with a copy
+-- and one L with a simple copy too!
+registerLR
+  p = x
+    ; x'
+  q = `NA_K x[0]
+    ; `NA_K x[0]
+
+registerL
+  p = `NA_I (Const 3)
+    ; `NA_I (Const 3)
+  q = ((:)  ((:>:)  y [] ) ((:)  ((:>:)  z [] ) [] ))
+    ; ((:)  ((:>:)  y' [] ) [] )
+
+----------------
+---------------
+----------------
+
+-- There must be some equality checking on LR; it's naturally
+-- ok for equal changes!
+registerLR
+  p = x
+    ; y
+  q = x
+    ; y
+
+----------------------
+----------------------
+----------------------
+
+-- the usual here
+registerLR
+  p = `NA_K x[2]
+    ; `NA_K x[2]
+  q = `NA_K x[4]
+    ; `NA_K x[4]
+
+-- But now it gets interesting! Again, we want to apply q to p.
+-- essentially duplicating p.
+registerR
+  p = ((:)  `NA_I (Const 0) ((:)  `NA_I (Const 1) [] ))
+    ; ((:)  `NA_I (Const 1) ((:)  `NA_I (Const 0) [] ))
+  q = `NA_I (Const 3)
+    ; ((:)  ((:>:)  y `NA_I (Const 3)) `NA_I (Const 3))
+
+-------------------
+-------------------
+-------------------
+
+registerLR
+  p = `NA_K x[5]
+    ; `NA_K x[5]
+  q = x
+    ; y
+
+registerLR
+  p = ((:>:)  a [] )
+    ; `NA_I (Const 4)
+  q = `NA_I (Const 4)
+    ; `NA_I (Const 4)
+
+-- Looks like I want to apply p to (cCtxIns q); call it Q;
+-- and return 'cmatch (cCtxDel p) Q'; looking back, this is also what
+-- i want on the other cases
+registerL
+  p = ((:)  `NA_I (Const 4) ((:)  ((:>:)  k [] ) `NA_I (Const 2)))
+    ; `NA_I (Const 2)
+  q = ((:)  `NA_I (Const 6) ((:)  `NA_I (Const 5) `NA_I (Const 2)))
+    ; ((:)  `NA_I (Const 6) ((:)  `NA_I (Const 5) ((:)  ((:>:)  new [] ) `NA_I (Const 2))))
+
+
+
+-}
+
 registerLR :: (C ki fam codes at)
            => Holes2 ki codes at
            -> Holes2 ki codes at
