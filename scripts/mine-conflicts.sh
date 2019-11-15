a7fb892c6835c12317e61e776f6e521668a1faaf
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2019-11-15 13:55:18 +0100

    Better repository miner

--- scripts/mine-conflicts.sh
+++ scripts/mine-conflicts.sh
@@ -5,7 +5,7 @@ GIT_WORK_TREE=""
 GIT_DIR=""
 
 function showUsage() {
-  echo "./mine-conflicts.sh path/to/repositories path/to/dataset/tgt language"
+  echo "./mine-conflicts.sh path/to/repository path/to/dataset/tgt language"
   echo ""
   echo "  Example usage: Mine all the conflicts for .el files in repository local/repos/helm"
   echo "  and place them in ./conflicts-el"
@@ -19,7 +19,7 @@ if [[ "$#" -ne "3" ]]; then
   exit 1
 fi
 
-repos="$1"
+repo="$1"
 target="$2"
 lang="$3"
 
@@ -35,46 +35,39 @@ function earlyexit {
 
 trap earlyexit SIGINT SIGTERM SIGQUIT
 
-for repo in $repos/*/; do
-  echo "Mining $repo"
-  old_branch=$(git -C $repo symbolic-ref --short HEAD)
+echo "Mining $repo"
+old_branch=$(git -C $repo symbolic-ref --short HEAD)
 
-  for commit in $(git -C $repo rev-list --merges HEAD); do
-    parents=$(git -C $repo log -1 --format=%P $commit)
-    fst=${parents%% *}
-    rest=${parents#* }
-    git -C $repo checkout -q $fst
-    git -C $repo merge --no-commit $rest >/dev/null 2>&1
-    if git -C $repo ls-files --unmerged | grep -q '^'; then
-      echo "found conflict in $repo - $commit"
-      unmerged=$(git -C $repo ls-files --unmerged | cut -d' ' -f2,3 | paste -d'\n' -s)
-      git -C $repo ls-files --unmerged
-      while read -r objID; do
-        obj=${objID:0:40}
-        role=${objID:41:1}
-        extension=${objID: -3}
-        if [ "$extension" == ".$lang" ]; then
-          if [ "$role" -eq "1" ]; then
-            # We set the $obj the first time
-            towrite="$target/$(basename $repo)-$commit-$obj"
-            fname="O.$lang"
-          fi
-          if [ "$role" -eq "2" ]; then
-            fname="A.$lang"
-          fi
-          if [ "$role" -eq "3" ]; then
-            fname="B.$lang"
-          fi
-          echo "$towrite"
-          mkdir -p "$towrite"
-          git -C $repo cat-file -p $obj > "$towrite/$fname"
-        fi
-      done <<< $unmerged
-      git -C $repo merge --abort
-    fi
-    git -C $repo reset -q --hard
-    git -C $repo clean -fdxq
-  done
-  git -C $repo checkout -q $old_branch
+for commit in $(git -C $repo rev-list --merges HEAD); do
+  parents=$(git -C $repo log -1 --format=%P $commit)
+  fst=${parents%% *}
+  rest=${parents#* }
+  git -C $repo checkout -q $fst
+  git -C $repo merge --no-commit $rest >/dev/null 2>&1
+  if git -C $repo ls-files --unmerged | grep -q '^'; then
+    echo "found conflict in $repo - $commit"
+    while read -r line; do
+      objO=$(echo $line | cut -d' ' -f 1)
+      objA=$(echo $line | cut -d' ' -f 2)
+      objB=$(echo $line | cut -d' ' -f 3)
+      fullname=$(echo $line | cut -d' ' -f 4)
+      extension=$(echo $fullname | cut -d. -f 2)
+      if [ "$extension" == "$lang" ]; then
+        towrite="$target/$(basename $repo)-$commit-$objO"
+        mkdir -p "$towrite"
+        git -C $repo cat-file -p $objO > "$towrite/O.$lang"
+        git -C $repo cat-file -p $objA > "$towrite/A.$lang"
+        git -C $repo cat-file -p $objB > "$towrite/B.$lang"
+        git -C $repo show $commit:$fullname > "$towrite/M.$lang"
+      fi
+    done < <( git -C $repo ls-files --unmerged \
+            | awk '{ if ($3 == 1) F1=$2; \
+                     if ($3 == 2) F2=$2; \
+                     if ($3 == 3) { F3=$2 ; print F1 " " F2 " " F3 " " $4 }}')
+    git -C $repo merge --abort
+  fi
+  git -C $repo reset -q --hard
+  git -C $repo clean -fdxq
 done
+git -C $repo checkout -q $old_branch
 
