f5f4a55ccabc49ec89611c2a0dd6a8b9472181b3
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2020-02-17 10:26:50 +0100

    Important bugfix; thanks alejandro; ordering the subsittuion is the way to go

--- src/Data/HDiff/Apply.hs
+++ src/Data/HDiff/Apply.hs
@@ -10,6 +10,7 @@ import Generics.Simplistic.Unify
 ------------------------------------
 import Data.HDiff.Base
 import Data.HDiff.Show
+import Data.HDiff.MetaVar
 
 
 patchApply :: Patch kappa fam at
@@ -23,6 +24,8 @@ chgApply :: Chg kappa fam at
          -> Maybe (SFix kappa fam at)
 chgApply chg p = 
   case runExcept $ unify (chgDel chg) (holesMapAnn (error "imp") id p) of
-    Left  err   -> error (show err)
+    Left  err   -> Nothing -- error (show err)
     Right sigma -> holesMapAnnM (const Nothing) return
                  $ substApply sigma (chgIns chg)
+ where
+   showAndErr mv = error ("non-instantiated-mvar: " ++ show (metavarGet mv))
