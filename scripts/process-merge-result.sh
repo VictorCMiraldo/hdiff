325e8d02915c65d5d3e934516686b89cd6462e6e
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2020-02-17 10:35:49 +0100

    MAde process-result works with multiple args

--- scripts/process-merge-result.sh
+++ scripts/process-merge-result.sh
@@ -2,20 +2,17 @@
 set -euo pipefail
 
 showUsage() {
-  echo "Usage: process-merge-result.sh <log-file>"
+  echo "Usage: process-merge-result.sh <log-file1> ... <log-fileN>"
   echo ""
   echo "  Log file must have been produced by ./run-experiment.sh ... merge.sh ..."
   echo "  Or be in the format:"
   echo ""  
   echo "  <reponame-commitId-objId> <height> <mode> <opq-mode> <global|local> <result>"
   echo ""
-  echo "  where <height>; <mode>; <opq-mode> and <global|local> must be /the same/ througout."
-  echo ""
-
   exit 1
 }
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -lt 1 ]]; then
   echo "Wron number of arguments"
   showUsage
 fi
@@ -28,10 +25,10 @@ awkScript='\
   /parse-error/ { PERR=$1  }\
   /timeout/     { TOUT=$1  }\
   /unknown/     { OTHER=$1 }\
-  END           { print " success+mdif: " (SUCC + MDIF) " mdiff: " MDIF " conf: " CONF " parse-error: " PERR " timeout: " TOUT " other: " OTHER }'
+  END           { print " success+mdiff: " (SUCC + MDIF) " mdiff: " MDIF " conf: " CONF " parse-error: " PERR " timeout: " TOUT " other: " OTHER }'
 
 while IFS= read -r header; do 
-  cat $1 | grep "$header" | cut -d' ' -f6 | sort | uniq -c | awk "$awkScript" | xargs -I{} echo "$header {}"
-done < <(cat $1 | cut -d' ' -f2,3,4,5 | sort | uniq)
+  cat "$@" | grep "$header" | cut -d' ' -f6 | sort | uniq -c | awk "$awkScript" | xargs -I{} echo "$header {}"
+done < <(cat "$@" | cut -d' ' -f2,3,4,5 | sort | uniq)
 
 
